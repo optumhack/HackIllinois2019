79cfedc06bda7d0db0b5fd05d5482d7d59d4b916
richard11235 <li.richard11235@gmail.com>
2019-02-22 23:23:34 -0600

    Added sections to work on

--- MakePaths.py
+++ MakePaths.py
@@ -1,1 +1,17 @@
 import numpy as np
+
+#Put employee information into usable Dictionary or object
+
+
+#Put patient information into usable Dictionary or object
+
+
+#Put task information into usable Dictionary or object
+
+
+#Run and parse API
+
+
+#Process data
+
+
