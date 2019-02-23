af55979208f615afa89f705e63ffe499d485fac4
richard11235 <li.richard11235@gmail.com>
2019-02-23 13:33:49 -0600

    Added bugfix to mat to properly process files with trailing newlines

--- mat.py
+++ mat.py
@@ -1,15 +1,22 @@
 import numpy as np
 class mat:
     def __init__(self, data):
+        height = len(data)
+        width = len(data[0])
+        for i in range(height -1, -1, -1):
+            if len(data[i]) < width:
+                del data[i]
         self.data = data
 
     def __getitem__(self, key):
         found = False
+        index = None
         for i in range(len(self.data[0])):
-            if self.data[0][i] == key:
+            if self.data[0][i].upper() == key.upper():
                 found = True
+                index = i
                 break
         if found == False:
             raise KeyError('Key not found')
-        return np.array([row[i] for row in self.data[1:]])
+        return np.array([row[index] for row in self.data[1:]])
 
