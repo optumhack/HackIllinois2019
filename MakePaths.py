27600c3747412b59241f6efcab56a838a1ae7bd7
richard11235 <li.richard11235@gmail.com>
2019-02-22 23:32:53 -0600

    API groundwork added

--- MakePaths.py
+++ MakePaths.py
@@ -8,13 +8,17 @@ import json
 
 #Put task information into usable Dictionary or object
 
-
 #Run and parse API
+key = 'Aoo6MSOdOhu7_gENweR-26VzV-fP83hR3kCT3EouCWeQdF_uyhsT2kx5ZWqyffI2'
+origins = '47.6044,-122.3345;47.6731,-122.1185;47.6149,-122.1936'
+destinations = '45.5347,-122.6231;47.4747,-122.2057'
+API_url = 'https://dev.virtualearth.net/REST/v1/Routes/DistanceMatrix?origins='+origins+'&destinations='+destinations+'&travelMode=driving&key='+key
+
+
 f = open('examplejson.txt','r')
 data_string = f.read()
-print(data_string)
-
-
+parsed_json = json.loads(data_string)
+print(parsed_json)
 #Process data
 
 
