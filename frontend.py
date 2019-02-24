d6064fae7c2c407a5ac8d5ff8dcdf7681606475d
girlt <edchang2@illinois.edu>
2019-02-23 22:21:24 -0600

    added the 'hours' button

--- frontend.py
+++ frontend.py
@@ -7,27 +7,48 @@ class Frontend:
         self.master = master
         master.title("Check schedule")
         self.id = 0
+        self.hours = 0
 
-        # widgets here
-        self.label = Label(master, text='Your Nurse ID')
+        # WIDGETS HERE
 
-        vcmd = master.register(self.validate) # we have to wrap the command
-        self.entry = Entry(master, validate="key", validatecommand=(vcmd, '%P'))
+        #nurse id
+        self.idLabel = Label(master, text='Your Nurse ID')
+        vcmdId = master.register(self.validateId) # we have to wrap the command
+        self.idEntry = Entry(master, validate="key", validatecommand=(vcmdId, '%P'))
 
+        #nurse hours
+        self.nurseLabel = Label(master, text='How Many Hours')
+        vcmdHours = master.register(self.validateHours)  # we have to wrap the command
+        self.nurseEntry = Entry(master, validate="key", validatecommand=(vcmdHours, '%P'))
+
+        #enter button
         self.button = Button(master, text="Enter", command=lambda: self.run())
 
         #layout
-        self.label.grid(row=0)
-        self.entry.grid(row=0, column=1)
-        self.button.grid(row=1, column=1)
+        self.idLabel.grid(row=0, column=0)
+        self.idEntry.grid(row=0, column=1)
+        self.nurseLabel.grid(row=1, column=0)
+        self.nurseEntry.grid(row=1, column=1)
+        self.button.grid(row=2, column=1)
 
-    def validate(self, new_text):
+    def validateId(self, new_text):
         if not new_text:
             self.id = 0
             return True
 
         try:
             self.id = int(new_text)
+            return True
+        except ValueError:
+            return False
+
+    def validateHours(self, new_text):
+        if not new_text:
+            self.hours = 0
+            return True
+
+        try:
+            self.hours = int(new_text)
             return True
         except ValueError:
             return False
